3b2d5898538827381041ff08891221b64872aa71
Jerrin Thomas <jerrinmachu@gmail.com>
2022-10-26 15:31:18 +0530

    Login and register is completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route} from "react-router-dom"
 import {Landing, Error, Register, Dashboard }from "./pages";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
@@ -11,6 +13,10 @@ function App() {
       <Route path = "*" element={<Error/>}/>
       
     </Routes>
+
+    <ToastContainer position="top-center">
+      
+    </ToastContainer>
    </BrowserRouter>
   );
 }
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
-import { Logo } from '../components';
+import { FormRow, Logo } from '../components';
 import Wrapper from '../assets/wrappers/RegisterPage';
+import {toast} from "react-toastify"
+import { useSelector, useDispatch} from 'react-redux'
+import { loginUser, registerUser} from "../features/user/userSlice"
+import { useNavigate } from "react-router-dom"
 // import styled from 'styled-components';
 
 const initialState = {
@@ -12,26 +16,66 @@ const initialState = {
 }
 
 const Register = () => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const { isLoading,user} = useSelector((store)=> store.user)
   const [ values, setValues] = useState(initialState)
 
+
+  useEffect(() => {
+    if(user){
+      setTimeout(() => {
+        navigate("/")
+      }, 3000);
+    }
+  
+   
+  }, [user])
+  
+
   const handleChange = (e) => {
-    console.log(e.target)
+    
+    const name = e.target.name;
+    const value = e.target.value;
+    console.log(`${name}: ${value}`)
+    setValues({ ...values, [name] : value })
+  }
+
+  const toggleMember = () => {
+    setValues({...values, isMember: !values.isMember})
   }
 
   const onSubmit = (e) => {
     e.preventDefault()
-    console.log(e.target) 
+    const { name, email, password, isMember } = values;
+    if(!email || !password || (!isMember && !name)){
+      toast.error('Please Fill out All fields');
+      return;
+    }
+
+    if(isMember){
+      dispatch(loginUser({ email:email, password: password }))
+      return;
+    }
+
+    dispatch(registerUser({name,email,password}))
   }
   return (
     <Wrapper className='full-page'>
       <form className='form' onSubmit={onSubmit}>
           <Logo/>
-          <h3>Login</h3>
-          <div className="form-row">
-              <label htmlFor='name' className='form-label'> name</label>
-              <input type='text' value={values.name} name='name' onChange={handleChange} className="form-input"/>
-          </div>
-          <button type='submit' className='btn btn-block'> submit</button>
+          
+
+         <h3> { values.isMember ? 'Login' : 'Register'} </h3>
+         
+        { !values.isMember && <FormRow type="text" name="name" value={values.name} handleChange={handleChange}/> }
+          
+          <FormRow type="email"name="email" value={values.email} handleChange={handleChange}/>
+          <FormRow type="password" name="password" value={values.password} handleChange={handleChange}/>
+          <p> { values.isMember ? 'Not a member yet ?' :'Already a member'}
+
+          <button type='button' onClick={toggleMember} className="member-btn"> { values.isMember ? 'Register':'Login'}</button></p>
+          <button type='submit' className='btn btn-block' disabled = {isLoading}> { isLoading ? 'Loading...' : 'Submit'}</button> 
       </form>
     </Wrapper>
   )
